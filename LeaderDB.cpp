e032ceb0076aae7069edad6d43aba8df3d0a260c
Zirui <wzrqczj@gmail.com>
2025-04-22 10:31:00 -0400

    delete StorageEngine module

--- LeaderDB.cpp
+++ LeaderDB.cpp
@@ -2,15 +2,15 @@
 
 void LeaderDB::set(const string& key, const vector<string>& attrs) {
     wal.logWrite(key, attrs); 
-    storage.set(key, attrs); 
+    //storage.set(key, attrs); 
     index.insert(key, attrs);
 }
 
 void LeaderDB::recoverFromWAL() {
-    storage.clear();
+    //storage.clear();
     auto entries = wal.loadLog();
     for (const auto& [key, val]: entries) {
-        storage.set(key, val);
+        //storage.set(key, val);
         vector<string> attrs = {val};  // 将 string 转换为 vector<string>
         index.insert(key, attrs);
     }
@@ -19,15 +19,21 @@ void LeaderDB::recoverFromWAL() {
 vector<string> LeaderDB::get(const string& key) {
     auto results = index.search(key);
     if (!results.empty()) {
-        return storage.get(key);
+        return results;
     }
     return {};
 }
 
 void LeaderDB::deleteKey(const string& key) {
-    storage.deleteKey(key);
+    //storage.deleteKey(key);
+    index.remove(key);
 }
 
 vector<vector<string>> LeaderDB::getPrefix(const string& prefixKey) {
-    return storage.getPrefix(prefixKey);
+    string high = prefixKey; high.push_back(CHAR_MAX);
+    vector<vector<string>> result;
+    for (const auto& item : index.rangeQuery(prefixKey, high)) {
+        result.push_back({item});
+    }
+    return result;
 }
--- LeaderDB.h
+++ LeaderDB.h
@@ -2,7 +2,7 @@
 #define LEADERDB_H
 
 #include "DBInstance.h"
-#include "StorageEngine.h"
+//#include "StorageEngine.h"
 #include "BTreeIndex.h"
 #include "WriteAheadLog.h"
 
@@ -11,7 +11,7 @@ using namespace std;
 //inherits publicly from DBInstance
 class LeaderDB : public DBInstance {
 private:
-    StorageEngine storage;
+    //StorageEngine storage;
     WriteAheadLog wal;
     BTreeIndex index;  
 
